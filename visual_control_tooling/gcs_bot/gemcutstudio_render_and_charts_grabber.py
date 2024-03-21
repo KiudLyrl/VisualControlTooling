53ee3006b7f13cb0bd7dc67c73c533563d522ad7
KiudLyrl <1658755+KiudLyrl@users.noreply.github.com>
2024-03-21 17:22:25 +0100

    greatly enhance gemcutstudio automation
    works 99.99% of the time now

--- visual_control_tooling/gcs_bot/gemcutstudio_render_and_charts_grabber.py
+++ visual_control_tooling/gcs_bot/gemcutstudio_render_and_charts_grabber.py
@@ -5,7 +5,7 @@ import os
 import shutil
 
 from visual_control_tooling.core.log_system import Logger
-from visual_control_tooling.core.vision_cortex import TemplateManager
+from visual_control_tooling.core.vision_cortex import TemplateManager, smart_ui_clic, clic_and_detect_color_change
 from visual_control_tooling.core.screen_area_management import get_gem_cut_studio_screen_area_params
 from visual_control_tooling.core.pc_interaction import Screenshotter, Cliquer
 from visual_control_tooling.core.data_models import Point
@@ -14,7 +14,7 @@ from visual_control_tooling.core.data_models import Point
 Example of how I automate softwares without API/command line interfaces
 """
 
-diagrams_path = r"C:\checkout2\common-scripts\facet_diagrams\facet_diagrams\ALL"
+diagrams_path = r"C:\checkout2\common-scripts\facet_diagrams\facet_diagrams\open_diagrams\diagrams"
 temp_folder_for_easy_load = r"C:\checkout2\common-scripts\facet_diagrams\facet_diagrams\temp_for_rendering"
 animation_output_path = r"C:\Users\Oliver\Documents\GemCutStudio\Animations"
 chart_output_path = r"C:\Users\Oliver\Documents\GemCutStudio\Designs"
@@ -23,6 +23,14 @@ file_button = Point(28, 36)
 open_button = Point(41, 106)
 first_file_tile = Point(223, 156)
 open_file_button = Point(1690, 1012)
+
+file_selection_tile_pixel_measure_color_pos = Point(475, 130)
+file_selection_tile_pixel_measure_color_inactive_color = (54, 54, 54)
+file_selection_tile_pixel_measure_color_active_color = (68, 97, 97)
+
+file_selection_open_button_pixel_measure_color_pos = Point(1650, 1009)
+file_selection_open_button_pixel_measure_color_active_color = (14, 14, 14)
+file_selection_open_button_pixel_measure_color_inactive_color = (64, 64, 64)
 
 #################
 ## LOGGER INIT ##
@@ -54,14 +62,10 @@ def copy_file_in_temp_folder(filename, temp_folder_path):
 
 def open_the_diagram_in_the_temp_folder(cliquer):
     logger.log_info("Opening the new diagram")
-    cliquer.click_relative_to_screen_area_and_return_to_last_pos(file_button)
-    time.sleep(0.5)
-    cliquer.click_relative_to_screen_area_and_return_to_last_pos(open_button)
-    time.sleep(0.5)
-    cliquer.click_relative_to_screen_area_and_return_to_last_pos(first_file_tile)
-    time.sleep(0.5)
-    cliquer.click_relative_to_screen_area_and_return_to_last_pos(open_file_button)
-    time.sleep(0.5)
+    smart_ui_clic(tm_top_menu_bar_file_button, tm_top_menu_bar_file_open_button, timeout=5)
+    smart_ui_clic(tm_top_menu_bar_file_open_button, tm_open_file_screen_indicator, timeout=5)
+    clic_and_detect_color_change(screenshotter, cliquer, gcs_screen_area_params, first_file_tile, file_selection_tile_pixel_measure_color_pos)
+    smart_ui_clic(tm_open_file_scren_open_button, tm_top_menu_bar_file_button, timeout=5)
 
 def is_animation_already_saved(file_name):
     anim_file_name = file_name[:-4] + ".gif"
@@ -75,6 +79,36 @@ def is_chart_already_saved(file_name):
     logger.log_info(f"  - chacking {chart_filename}")
     return os.path.exists(chart_filename)
 
+def open_tilt_perf():
+    smart_ui_clic(tm_top_menu_bar_tools_button, tm_top_menu_bar_tools_tiltperf_button, timeout=2)
+    smart_ui_clic(tm_top_menu_bar_tools_tiltperf_button, tm_export_anim_button_1, timeout=2)
+
+def save_animation_if_needed():
+    if not is_animation_already_saved(diagram_file_name):
+
+        smart_ui_clic(tm_export_anim_button_1, tm_save_anim_button_2, timeout=200)
+        smart_ui_clic(tm_save_anim_button_2, tm_os_save_button)
+        tm_os_save_button.clic_on_template_if_present_untill_it_disseapear()
+
+        # we can use smart_ui_clic because that close button is visible while a loading bar is being filled, should templetize something that is masked by the bar
+        # but unfortunatly there is nothing behind that bar so we can only block untill it disseapear
+        tm_tilperf_creategif_dialog.block_WHILE_template_is_present(0.2, 200)
+
+        smart_ui_clic(tm_anim_popup_close_button, tm_tilt_perf_presence_indicator)
+    else:
+        logger.log_info("  - Animation already saved")
+
+def save_graph_if_needed():
+    # save graph
+    if not is_chart_already_saved(diagram_file_name):
+        tm_save_graph_button.block_until_template_is_present(0.5, 5)
+        tm_save_graph_button.clic_on_template_if_present_only_once()
+
+        tm_os_save_button.block_until_template_is_present(0.5, 30)
+        tm_os_save_button.clic_on_template_if_present_only_once()
+    else:
+        logger.log_info("  - chart already saved")
+
 
 ## LOCATE GEMCUT STUDIO
 gcs_screen_area_params = get_gem_cut_studio_screen_area_params()
@@ -87,16 +121,20 @@ with mss.mss() as sct:
     tm_top_menu_bar_file_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/7,4_61,55_name=menufilebutton_precision=0.8_priority=7.png", "out_screenshots")
     tm_top_menu_bar_file_open_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/9,87_116,127_name=menuopenbutton_precision=0.8_priority=7.png", "out_screenshots")
 
+    # open file screen templates
+    tm_open_file_screen_indicator = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/60,40_400,128_name=openfilescreenindicator_precision=0.8_priority=7.png", "out_screenshots")
+    tm_open_file_scren_open_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/1570,954_1797,1050_name=opengilescreenopenbutton_precision=0.8_priority=7.png", "out_screenshots")
+
     # tools > ... # templates
     tm_top_menu_bar_tools_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/201,10_275,55_name=menutoolbutton_precision=0.8_priority=7.png", "out_screenshots")
-    tm_top_menu_bar_tools_tiltperf_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/208,51_437,98_name=menutiltperfbutton_precision=0.8_priority=7.png", "out_screenshots")
+    tm_top_menu_bar_tools_tiltperf_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/184,36_503,178_name=menutoolstiltperfbutton_precision=0.8_priority=7.png", "out_screenshots")
 
     # tilt performances buttons
+    tm_tilt_perf_presence_indicator = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/881,414_1022,671_name=tiltperfpresenceindicator_precision=0.8_priority=7.png", "out_screenshots")
     tm_os_save_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/397,182_1720,1000_name=oswindowsavebutton_precision=0.8_priority=7.png", "out_screenshots")
     tm_export_anim_button_1 = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/544,792_944,887_name=exportanimbutton1_precision=0.8_priority=7.png", "out_screenshots")
     tm_save_anim_button_2 = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/869,832_1058,912_name=animpopupsavebutton_precision=0.8_priority=7.png", "out_screenshots")
-    tm_anim_popup_close_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer,
-                                                 "templates/1113,285_1179,331_name=animpopupclosebutton_precision=0.95_priority=7.png", "out_screenshots")
+    tm_anim_popup_close_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/1113,285_1179,331_name=animpopupclosebutton_precision=0.95_priority=7.png", "out_screenshots")
     tm_save_graph_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/1134,820_1366,881_name=tiltperfwindowsavegraphbutton_precision=0.8_priority=7.png", "out_screenshots")
     tm_tiltperf_close_button = TemplateManager(gcs_screen_area_params, screenshotter, cliquer, "templates/1292,300_1387,375_name=tiltperfwindowclosebutton_precision=0.8_priority=7.png", "out_screenshots")
 
@@ -115,39 +153,9 @@ with mss.mss() as sct:
 
         copy_file_in_temp_folder(diagram_filename, temp_folder_for_easy_load)
         open_the_diagram_in_the_temp_folder(cliquer)
-
-        # open tilt perf
-        tm_top_menu_bar_tools_button.clic_on_template_if_present_only_once()
-        tm_top_menu_bar_tools_tiltperf_button.block_until_template_is_present(0.5, 5)
-        tm_top_menu_bar_tools_tiltperf_button.clic_on_template_if_present_only_once()
-
-        # save animation
-        if not is_animation_already_saved(diagram_file_name):
-            tm_export_anim_button_1.block_until_template_is_present(0.5, 5)
-            tm_export_anim_button_1.clic_on_template_if_present_only_once()
-
-            tm_save_anim_button_2.block_until_template_is_present(0.5, 30)
-            tm_save_anim_button_2.clic_on_template_if_present_only_once()
-
-            tm_os_save_button.block_until_template_is_present(0.5, 30)
-            tm_os_save_button.clic_on_template_if_present_only_once()
-
-            tm_tilperf_creategif_dialog.block_until_template_is_present(1, 30)
-
-            tm_anim_popup_close_button.block_until_template_is_present(0.5, 30)
-            tm_anim_popup_close_button.clic_on_template_if_present_only_once()
-        else:
-            logger.log_info("  - Animation already saved")
-
-        # save graph
-        if not is_chart_already_saved(diagram_file_name):
-            tm_save_graph_button.block_until_template_is_present(0.5, 5)
-            tm_save_graph_button.clic_on_template_if_present_only_once()
-
-            tm_os_save_button.block_until_template_is_present(0.5, 30)
-            tm_os_save_button.clic_on_template_if_present_only_once()
-        else:
-            logger.log_info("  - chart already saved")
+        open_tilt_perf()
+        save_animation_if_needed()
+        save_graph_if_needed()
 
         # close tilt perf
         tm_tiltperf_close_button.block_until_template_is_present(0.1, 5)
